135955909991d9d4981ba06c16b228f2f99f9cdf
endikaouvina <endikaouvina@gmail.com>
2026-06-02 13:30:10 +0200

    Añadido el stopPropagation en la ruta del mapa

--- js/modules/map.js
+++ js/modules/map.js
@@ -1,10 +1,19 @@
 'use strict';
 
+// Variables generales
 let options = {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 0,
 };
+
+// Elementos del DOM
+let divRuta;
+
+// Función para cargar los elementos del DOM
+function cargarElementosDOMMapa() {
+    divRuta = document.querySelectorAll('.leaflet-routing-container');
+}
 
 // Función para mostrar el mapa y la ruta si el usuario acepta compartir su ubicación
 function success(position) {
@@ -15,7 +24,7 @@ function success(position) {
     // Creamos el mapa
     let map = L.map('map');
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors',
     }).addTo(map);
 
     // Añadimos la ruta al mapa
@@ -23,6 +32,10 @@ function success(position) {
         waypoints: [L.latLng(latitude, longitude), L.latLng(40.03233455, -5.7437511)],
         language: 'es',
     }).addTo(map);
+
+    // Evitamos la propagación de scroll y click en la ruta
+    cargarElementosDOMMapa();
+    evitarPropagacion();
 }
 
 // Función para mostrar el mapa si el usuario no acepta compartir su ubicación
@@ -35,11 +48,20 @@ function error() {
 
     // Creamos el mapa
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors',
     }).addTo(map);
 
     // Añadimos un marcador para la ubicación de la empresa
     L.marker([40.03233455, -5.7437511]).addTo(map);
+}
+
+// Función para evitar la propagación de scroll y click en la ruta
+function evitarPropagacion() {
+    divRuta.forEach((div) => {
+        console.log(div);
+        L.DomEvent.disableScrollPropagation(div);
+        L.DomEvent.disableClickPropagation(div);
+    });
 }
 
 // Función "main" para la generación del mapa
