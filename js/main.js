5d00ce8661d55a6bd4a3040b7ba430ac74f5d475
endikaouvina <endikaouvina@gmail.com>
2026-06-05 18:56:34 +0200

    Todos los links absolutos cambiados a relativos

--- js/main.js
+++ js/main.js
@@ -7,16 +7,13 @@ import { prepararFormulario } from './modules/formValidation.js';
 import { prepararPresupuesto } from './modules/presupuesto.js';
 
 let urlPagina = window.location.href;
-if (urlPagina.includes('index.html')) {
-    cargarNoticias();
-}
 if (urlPagina.includes('galeria.html')) {
     iniciarCarousel();
-}
-if (urlPagina.includes('contacto.html')) {
+} else if (urlPagina.includes('contacto.html')) {
     generarMapa();
-}
-if (urlPagina.includes('presupuesto.html')) {
+} else if (urlPagina.includes('presupuesto.html')) {
     prepararFormulario();
     prepararPresupuesto();
+} else {
+    cargarNoticias();
 }
