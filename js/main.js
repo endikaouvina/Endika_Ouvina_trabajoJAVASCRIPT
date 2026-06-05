a28c71ba41fca34cb446b89f4090d44e6c97b61d
endikaouvina <endikaouvina@gmail.com>
2026-06-05 18:14:47 +0200

    Añadida la página de inicio

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 'use strict';
 
+import { cargarNoticias } from './modules/json.js';
+import { iniciarCarousel } from './modules/carousel.js';
+import { generarMapa } from './modules/map.js';
 import { prepararFormulario } from './modules/formValidation.js';
 import { prepararPresupuesto } from './modules/presupuesto.js';
-import { generarMapa } from './modules/map.js';
-import { iniciarCarousel } from './modules/carousel.js';
 
-$(document).ready(function () {
-    let urlPagina = window.location.href;
-    if (urlPagina.includes('galeria.html')) {
-        iniciarCarousel();
-    }
-    if (urlPagina.includes('contacto.html')) {
-        generarMapa();
-    }
-    if (urlPagina.includes('presupuesto.html')) {
-        prepararFormulario();
-        prepararPresupuesto();
-    }
-});
+let urlPagina = window.location.href;
+if (urlPagina.includes('index.html')) {
+    cargarNoticias();
+}
+if (urlPagina.includes('galeria.html')) {
+    iniciarCarousel();
+}
+if (urlPagina.includes('contacto.html')) {
+    generarMapa();
+}
+if (urlPagina.includes('presupuesto.html')) {
+    prepararFormulario();
+    prepararPresupuesto();
+}
---
+++ js/modules/json.js
@@ -0,0 +1,102 @@
+'use strict';
+
+// Variables globales
+const URL_NOTICIAS = '/resources/data/noticias.json';
+
+// Elementos del DOM
+let main;
+
+// Función para cargar los elementos del DOM
+function cargarElementosDOMJSON() {
+    main = document.querySelector('main');
+}
+
+// Función para crear las cartas
+function createJSONCard(item) {
+    // Creamos los elementos necesarios
+    const card = document.createElement('div');
+    const imgDiv = document.createElement('div');
+    const img = document.createElement('img');
+    const body = document.createElement('div');
+    const title = document.createElement('h3');
+    const text = document.createElement('p');
+    const button = document.createElement('a');
+
+    // Agregamos los atributos necesarios
+    img.src = item.src;
+    img.alt = item.alt;
+    button.href = item.href;
+
+    // Agregamos las clases necesarias
+    card.classList.add('col', 'card', 'mx-3');
+    imgDiv.classList.add('h-100', 'd-flex', 'flex-column', 'justify-content-center');
+    img.classList.add('card-img');
+    body.classList.add('card-body');
+    title.classList.add('card-title');
+    text.classList.add('card-text');
+    button.classList.add('btn', 'btn-secondary');
+
+    // Agregamos el contenido
+    title.textContent = item.title;
+    text.textContent = item.text;
+    button.textContent = item.buttonText;
+
+    // Construimos la estructura de la tarjeta
+    imgDiv.appendChild(img);
+    body.appendChild(title);
+    body.appendChild(text);
+    body.appendChild(button);
+    card.appendChild(imgDiv);
+    card.appendChild(body);
+
+    // Devolvemos la tarjeta
+    return card;
+}
+
+// Función para crear el contenido
+function createJSONContent(json) {
+    // Creamos los elementos necesarios
+    const section = document.createElement('section');
+    const container = document.createElement('div');
+    const title = document.createElement('h2');
+    const row = document.createElement('div');
+
+    // Agregamos las clases necesarias
+    container.classList.add('container');
+    row.classList.add('row');
+
+    // Agregamos el contenido
+    title.textContent = 'DESCUBRE NUESTRO CONTENIDO';
+
+    // Construimos la estructura del contenido
+    json.forEach((item) => {
+        row.appendChild(createJSONCard(item));
+    });
+    container.appendChild(title);
+    container.appendChild(row);
+    section.appendChild(container);
+
+    // Devolvemos el contenido
+    return section;
+}
+
+// Función para cargar el JSON
+async function cargarNoticias(params) {
+    try {
+        const respuesta = await fetch(URL_NOTICIAS);
+        if (!respuesta.ok) {
+            throw new Error('Error HTTP:', respuesta.status);
+        }
+        const json = await respuesta.json();
+
+        // Cargamos el contenido del JSON, creamos el contenido HTML y lo insertamos en la página
+        cargarElementosDOMJSON();
+        let content = createJSONContent(json);
+        main.insertBefore(content, main.children[0]);
+    } catch (error) {
+        console.error('Error al cargar el JSON:', error);
+    }
+}
+
+// Declaramos las exportaciones necesarias
+export { cargarNoticias };
